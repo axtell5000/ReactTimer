62f39375a7b3fb2d14c103dd2cf599f988065728
Stephen Axtell <axtell.stephen@gmail.com>
2017-03-19 15:48:58 +0200

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+
+var Nav = () => {
+
+
+    return (
+      <div className="top-bar">
+
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Time App</li>
+            <li><IndexLink to='/' activeClassName="active-link" >Timer</IndexLink></li>
+            <li><Link to='/' activeClassName="active-link">Countdown</Link></li>
+
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://github.com/axtell5000" target="_blank">Stephen Axtell</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    );
+
+
+};
+
+module.exports = Nav;
+
