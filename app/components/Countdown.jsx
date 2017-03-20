f94cdac3e8a4e156496913d86eaa09078449ffdd
Stephen Axtell <axtell.stephen@gmail.com>
2017-03-20 16:41:46 +0200

    Add page stubs and routes

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+
+  render: function () {
+
+     return (
+       <p>In Countdown.jsx</p>
+     );
+
+  }
+
+});
+
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Timer = React.createClass({
+
+  render: function () {
+
+    return (
+      <p>In Timer.jsx</p>
+    );
+
+  }
+
+});
+
+module.exports = Timer;
