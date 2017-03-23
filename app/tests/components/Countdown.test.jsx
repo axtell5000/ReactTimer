0344f0d42ae469666e5925d804b3522b577a3c5d
Stephen Axtell <axtell.stephen@gmail.com>
2017-03-23 12:16:28 +0200

    Adding countdown test and logic checking

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,43 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDom = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to started and countdown', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done(); //done has to be used when doing asynchronously function when using mocha
+      }, 1001)
+    });
+
+    it('should check if countdown does not go below 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done(); //done has to be used when doing asynchronously function when using mocha
+      }, 3001)
+    });
+
+
+
+  });
+
+});
